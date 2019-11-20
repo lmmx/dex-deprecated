f4f9c45e1e228c4179dac20864e9f6a1022c0e42
Louis Maddox <louismmx@gmail.com>
2019-11-20 18:01:05 +0000

    Link the book list

--- src/document_shelves.py
+++ src/document_shelves.py
@@ -25,7 +25,7 @@ def make_author_entry(auth):
         acf = cp.ConfigParser()
         acf.read(auth_info)
         first, last = acf['NAME']['first_names'], acf['NAME']['surname']
-        auth_entry = [f"\n:book: {last}, {first}:"]
+        auth_entry = [f"\n ### [:book: {last}, {first}]({auth}):"]
     else:
         auth_entry = [f"\n- {auth}"]
     auth_year_list = [x.name for x in (shelves_dir / auth).iterdir() if x.is_dir()]
